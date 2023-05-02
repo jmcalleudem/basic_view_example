86ddf592c14a03ae0844a9ef663f14d9afd8328a
Johnathan Calle <jmcalle@udemedellin.edu.co>
2023-05-02 15:34:04 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace basic_view_example.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
